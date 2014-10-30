f25ed665391bb55777f4a3e2f1aa46baef7cd9b4
cen5bin <cen5bin@163.com>
2014-10-30 14:11:40 +0800

    加入日志头文件

--- inverted_index/src/main.cpp
+++ inverted_index/src/main.cpp
@@ -1,9 +1,12 @@
 #include <cstdio>
 #include <cstring>
+#include "IRLog.h"
 
 unsigned char s[100];
 int main()
 {
+	LOG("%s", "岑武斌");
+	LERR("%s", "err");
 	while (gets((char *)s))
 	{
 		for (int i = 0; s[i]; i++) printf("%d ", s[i]);
